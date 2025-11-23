15d966351a0deb41480121f564e4bdd67e5eac6a
Ernela <fenixoffc@gmail.com>
2025-11-23 21:02:46 +0200

    small edits

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,22 @@
 plugins {
     kotlin("jvm") version "2.1.0"
     id("maven-publish")
+    id("java-library")
 }
+
+group = "com.github.Ernous"
+version = "1.0.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+    withSourcesJar()
+    withJavadocJar()
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "17"
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
@@ -31,13 +37,59 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.1")
 }
 
+// Configure tests
+tasks.test {
+    useJUnit()
+    testLogging {
+        events("passed", "skipped", "failed")
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        showStandardStreams = false
+    }
+}
+
+// Publishing configuration for JitPack
 publishing {
     publications {
-        create<MavenPublication>("maven") {
+        create<MavenPublication>("release") {
             from(components["java"])
-            groupId = "com.github.Ernous"
-            artifactId = "players-parser"
-            version = "1.0.0"
+            
+            pom {
+                name.set("Players Parser")
+                description.set("Kotlin library for parsing video player streams from various sources")
+                url.set("https://github.com/Ernous/players-parser")
+                
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                
+                developers {
+                    developer {
+                        id.set("Ernous")
+                        name.set("Ernous")
+                        url.set("https://github.com/Ernous")
+                    }
+                }
+                
+                scm {
+                    connection.set("scm:git:https://github.com/Ernous/players-parser.git")
+                    developerConnection.set("scm:git:https://github.com/Ernous/players-parser.git")
+                    url.set("https://github.com/Ernous/players-parser")
+                }
+            }
+        }
+    }
+    
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Ernous/players-parser")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user") as? String
+                password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.key") as? String
+            }
         }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,17 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
 rootProject.name = "players-parser"
+
+// Project configuration
+include(":players-parser")
